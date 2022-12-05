d579c4573562cb8c77915cd58d09d02448733b5e
Rajvinder Singh <ic21b088@technikum-wien.at>
2022-12-05 20:24:17 +0100

    First Commit

---
+++ ConverterProject/src/main/java/at/fhtw/ConverterProject/ConverterProjectApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.ConverterProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterProjectApplication.class, args);
+	}
+
+}
---
+++ ConverterProject/src/test/java/at/fhtw/ConverterProject/ConverterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.ConverterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
